46fdbb3fdf43995234ddba5b31af26db1f094473
SQL2006 <160690048@qq.com>
2026-06-16 23:03:54 +0800

    HTML版本

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"/FJK/"
+  base: '/FJK/'  // 加上这一行！仓库名是什么就写什么
 })
